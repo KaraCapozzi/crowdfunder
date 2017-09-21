9e4ca49392ec209e007d04d50657ee012ae5c782
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-09-21 11:19:40 -0400

    add date future validation to project model with passing test

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,11 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :user, :title, :description, :goal, :start_date, :end_date, presence: true
+  validate :start_date_future
+
+  def start_date_future
+    if start_date < Date.today
+      errors.add(:start_date, message:"Start date should be in the future.")
+    end
+  end
 end
